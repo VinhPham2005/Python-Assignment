a4672887f9da9652f42da203e801067c90259d99
VinhPham2005 <154905948+VinhPham2005@users.noreply.github.com>
2025-05-10 17:16:34 +0700

    Update estimating_player_values.py

--- SourceCode/Problem4/estimating_player_values.py
+++ SourceCode/Problem4/estimating_player_values.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import joblib
 
-df = pd.read_csv('Problem4/player_values.csv', na_values= 'N/a')
+df = pd.read_csv('SourceCode/Problem4/player_values.csv', na_values= 'N/a')
 df.fillna(0, inplace=True)
 
 df['Values'] = df['Values'].str.replace('€', '').str.replace('M', '').astype(float)
@@ -30,5 +30,5 @@ y_pred = model.predict(x_test)
 
 print('RMSE:', mean_squared_error(y_test, y_pred))
 print('r2 score:', r2_score(y_test, y_pred))
-joblib.dump(model, 'Problem4/linear_model.pkl')
+joblib.dump(model, 'SourceCode/Problem4/linear_model.pkl')
 
